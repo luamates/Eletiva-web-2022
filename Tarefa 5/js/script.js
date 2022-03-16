076104bad407de529939a58f5d42af33d295d170
luamates <lucas.amates@gmail.com>
2022-03-16 18:20:16 -0300

    input

--- Tarefa 5/js/script.js
+++ Tarefa 5/js/script.js
@@ -1,6 +1,10 @@
 var prods = [
-    { id: 1, name: "Produto 1", price: 10.0 },
-    { id: 1, name: "Produto 2", price: 20.0 },
+    { id: 1, name: "Produto 1", price: 30.0 },
+    { id: 2, name: "Produto 2", price: 25.0 },
+    { id: 3, name: "Produto 2", price: 22.0 },
+    { id: 4, name: "Produto 2", price: 10.0 },
+    { id: 5, name: "Produto 2", price: 8.0 },
+    { id: 6, name: "Produto 2", price: 12.0 },
 ];
 
 function calc(){
