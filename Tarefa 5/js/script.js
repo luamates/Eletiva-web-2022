86648010dadc950b9f7497a36396d4cd0436af3f
luamates <lucas.amates@gmail.com>
2022-03-16 22:33:56 -0300

    script

--- Tarefa 5/js/script.js
+++ Tarefa 5/js/script.js
@@ -1,21 +1,31 @@
 var prods = [
-    { id: 1, name: "Produto 1", price: 30.0 },
-    { id: 2, name: "Produto 2", price: 25.0 },
-    { id: 3, name: "Produto 2", price: 22.0 },
-    { id: 4, name: "Produto 2", price: 10.0 },
-    { id: 5, name: "Produto 2", price: 8.0 },
-    { id: 6, name: "Produto 2", price: 12.0 },
+    { id: 1, name: "Bife com batatas", price: 30.0 },
+    { id: 2, name: "Coxa de frango crocante", price: 25.0 },
+    { id: 3, name: "Carne de panela", price: 22.0 },
+    { id: 4, name: "Farofa", price: 10.0 },
+    { id: 5, name: "Salada", price: 8.0 },
+    { id: 6, name: "Torresmo", price: 12.0 },
 ];
 
-function calc(){
+function calc(){ 
 
        
     saida.innerHTML = '';
     var quantities = document.getElementsByName("quantity");
     
-    for (let input of quantities) {
-        saida.innerHTML += ` Produto: ${prods[input.id-1].name}  - Preço: ${prods[input.id-1].price} - Quantidade: ${input.value} </br>`
-    }
+   
+        alert(document.getElementById('customerName').value)
+
+        let cliNome = document.getElementById('customerName').value
+
+        saida.innerHTML += "Caro <b>" + cliNome + "</b><br><br> Seguem os dados do seu pedido. <br><br> O seu pedido é: <br><br>"
+
+        for (let input of quantities) {
+            saida.innerHTML += ` Produto: ${prods[input.id-1].name}  - Preço: ${prods[input.id-1].price} - Quantidade: ${input.value} </br>`
+        }
+    
+
+
 
     
 }
