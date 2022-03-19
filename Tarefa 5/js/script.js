1c845c35ef4d3cbee6f44031c1fe41d661998407
luamates <lucas.amates@gmail.com>
2022-03-18 23:09:56 -0300

    script

--- Tarefa 5/js/script.js
+++ Tarefa 5/js/script.js
@@ -1,45 +1,57 @@
-var prods = [
-    { id: 1, name: "Bife com batatas"},
-    { id: 2, name: "Coxa de frango crocante"},
-    { id: 3, name: "Carne de panela"},
-    { id: 4, name: "Farofa"},
-    { id: 5, name: "Salada"},
-    { id: 6, name: "Torresmo"},
+var prods = [{
+        id: 1,
+        name: "Bife com batatas"
+    },
+    {
+        id: 2,
+        name: "Coxa de frango crocante"
+    },
+    {
+        id: 3,
+        name: "Carne de panela"
+    },
+    {
+        id: 4,
+        name: "Farofa"
+    },
+    {
+        id: 5,
+        name: "Salada"
+    },
+    {
+        id: 6,
+        name: "Torresmo"
+    },
 ];
 
 var thePrices = [30, 25, 22, 10, 8, 12]
 
-function start(){
+function start() {
     saida.innerHTML = '';
-    
+
     var customerName, customerNumber, customerEmail;
-   
+
     customerName = document.getElementById('customerName').value
     customerNumber = document.getElementById('phone').value
     customerEmail = document.getElementById('email').value
 
-    if(customerName == "" || customerNumber == "" || customerEmail == "")
-    {
+    if (customerName == "" || customerNumber == "" || customerEmail == "") {
         alert("Por favor, insira todos os dados.");
-    }
-    else
-    {
+    } else {
         saida.innerHTML += "Caro <b>" + customerName + "</b><br><br> Seguem os dados do seu pedido. <br><br> O seu pedido é: <br><br>"
         var finalPrice = 0
 
-        for(i = 0 ; i < 6 ; i++)
-        {
+        for (i = 0; i < 6; i++) {
             idBYi = i + 1;
             quant = document.getElementById(idBYi).value
-            
-            if(quant != 0)
-            {
+
+            if (quant != 0) {
                 var totalPrice = thePrices[i] * quant
                 finalPrice += totalPrice;
 
-                saida.innerHTML += "<li>Prato: " + prods[i].name + " - Preço unitário: R$ " + thePrices[i] + " - Quantidade: " + quant + " - Total: R$ " + totalPrice;
+                saida.innerHTML += "<li>Prato: " + prods[i].name + " - Preço unitário: R$ " + thePrices[i] + ",00 - Quantidade: " + quant + " - Total: R$ " + totalPrice + ",00";
             }
         }
-        saida.innerHTML += "<br><br><b>Preço final R$ " + finalPrice + "<b>";
+        saida.innerHTML += "<br><br><b>Preço final R$ " + finalPrice + ",00<b>";
     }
 }
